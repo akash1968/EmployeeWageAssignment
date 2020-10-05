6549acc38ce9f091fc8a4a189eacbbafada3640c
Akash <akash.singh670@gmail.com>
2020-10-05 19:56:26 +0530

    [Akash].[Refractor].Added UC4

--- Program.cs
+++ Program.cs
@@ -6,24 +6,25 @@ namespace UC1
     {
         static void Main(string[] args)
         {
-            int EMP_PART_TIME = 1;
-            int EMP_FULL_TIME = 2;
+            Console.WriteLine("Use case 4");
+            const int EMP_PART_TIME = 1;
+            const int EMP_FULL_TIME = 2;
             int EMP_RATE_PER_HR = 20;
             int empHours = 0;
             int empWage = 0;
             Random random = new Random();
             int empCheck = random.Next(0, 3);
-            if (empCheck == EMP_FULL_TIME)
+            switch (empCheck)
             {
-                empHours = 8;
-            }
-            else if (empCheck == EMP_PART_TIME)
-            {
-                empHours = 4;
-            }
-            else
-            {
-                empHours = 0;
+                case EMP_FULL_TIME:
+                    empHours = 8;
+                    break;
+                case EMP_PART_TIME:
+                    empHours = 4;
+                    break;
+                default:
+                    empHours = 0;
+                    break;
             }
             empWage = empHours * EMP_RATE_PER_HR;
             Console.WriteLine("Employee Wage per hour is.. " + empWage);
@@ -31,4 +32,3 @@ namespace UC1
         }
     }
 }
-
