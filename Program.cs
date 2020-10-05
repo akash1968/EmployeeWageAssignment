e3fc38a23359a228bce7a1da7f4e4c685acc1c92
Akash <akash.singh670@gmail.com>
2020-10-05 19:30:05 +0530

    [Akash] .Added UC1 of Employee Wage

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace UC1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int isPresent = Attendance();
+            if (isPresent == 0)
+                Console.WriteLine("Employee is Absent!");
+            else
+                Console.WriteLine("Employee is Present!");
+        }
+
+        static int Attendance()
+        {
+            Random randObj = new Random();
+            return randObj.Next(0, 2);
+        }
+
+    }
+}
